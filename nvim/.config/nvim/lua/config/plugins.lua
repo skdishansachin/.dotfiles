f03824a1ff5632b1cd1c755dca42256ae605bb95
skdishansachin <skdishansachin@gmail.com>
2025-07-24 14:00:00 +0530

    formatted

--- nvim/.config/nvim/lua/config/plugins.lua
+++ nvim/.config/nvim/lua/config/plugins.lua
@@ -1,75 +1,75 @@
 vim.opt.rtp:prepend("~/.config/nvim/lazy/lazy.nvim")
 
 require("lazy").setup({
-  { "neovim/nvim-lspconfig" },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = {}
-  },
-  { "EdenEast/nightfox.nvim" },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = true,
-  },
-  { "github/copilot.vim" },
-  { "tpope/vim-surround" },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          side = "right",
-        },
-        filters = {
-          dotfiles = false,
-        },
-      })
-    end,
-  },
-  {
-      "rust-lang/rust.vim",
-      ft = { "rust" },
-      init = function()
-        vim.g.rustfmt_autosave = 1
-      end,
-  },
-  {
-      'stevearc/conform.nvim',
-      config = function()
-          require('conform').setup({
-              formatters_by_ft = {
-                  go = { "goimports", "gofmt" },
-                  lua = { "stylua" },
-              }
-          })
-      end,
-  }
+	{ "neovim/nvim-lspconfig" },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{ "EdenEast/nightfox.nvim" },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = true,
+	},
+	{ "github/copilot.vim" },
+	{ "tpope/vim-surround" },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({
+				view = {
+					side = "right",
+				},
+				filters = {
+					dotfiles = false,
+				},
+			})
+		end,
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = { "rust" },
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					go = { "goimports", "gofmt" },
+					lua = { "stylua" },
+				},
+			})
+		end,
+	},
 })
