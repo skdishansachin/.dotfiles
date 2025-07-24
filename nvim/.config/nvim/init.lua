943220542a9b499e9948757896a4ae6b0e5d542a
skdishansachin <skdishansachin@gmail.com>
2025-07-24 13:37:06 +0530

    config for conform.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,14 +8,22 @@ require("config.keymaps")
 vim.cmd("colorscheme carbonfox")
 
 require("toggleterm").setup({
-    direction = "horizontal",
-    size = 15,
-    open_mapping = [[<leader>t]],
-    start_in_insert = true,
-    insert_mappings = true,
-    shade_terminals = true
+	direction = "horizontal",
+	size = 15,
+	open_mapping = [[<leader>t]],
+	start_in_insert = true,
+	insert_mappings = true,
+	shade_terminals = true,
 })
 
 -- Copilot configuration
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { expr = true, silent = true, noremap = true })
+
+-- Conform.nvim configuration
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
