1aff38c0670e7548b7540f70bf5a06c5e2c21357
skdishansachin <skdishansachin@gmail.com>
2025-07-25 12:32:22 +0530

    nvim from sketch

--- nvim/.config/nvim/lua/config/options.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.termguicolors = true
